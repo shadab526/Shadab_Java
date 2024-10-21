8e3fb5acee7a815758d4a71614580574fa7217bf
shadab526 <sshadab526@gmail.com>
2024-10-22 04:54:29 +0530

    21 Oct 2024 Task

---
+++ src/Java_Practice/switchCase/ex001.java
@@ -0,0 +1,11 @@
+package Java_Practice.switchCase;
+import java.util.Scanner;
+public class ex001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the time to watch movie");
+        int time = sc.nextInt();
+
+
+    }
+}
